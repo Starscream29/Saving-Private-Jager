d6c0b128b127e6c29f26fff04d899381902376fb
Scott Chang <37944809+Starscream29@users.noreply.github.com>
2018-03-31 11:12:57 -0600

    Add files via upload

--- SPJ.py
+++ SPJ.py
@@ -1,5 +1,6 @@
 # import the pygame module, so you can use it
 import pygame
+from Functions import constrain
 
 
 # define a main function
@@ -14,20 +15,19 @@ def main():
     pygame.display.set_caption("Scott Chang looks for his son")
 
     # create a surface on screen that has the size of 240 x 180
-    screenWidth = 1800
-    screenHeight = 800
+    screenWidth = 1300
+    screenHeight = 650
     screen = pygame.display.set_mode((screenWidth, screenHeight))
 
     background = pygame.image.load("background.png")
     screen.blit(background, (0, 0))
 
-    # define the position of the smily
-    ScottX = 250
-    ScottY = 250
-    # how many pixels we move our smily each frame
-    stepX = 50
-    stepY = 50
-    # check if the smily is still on screen, if not change direction
+    # define scott's initial position
+    ScottX = 650
+    ScottY = 300
+    # how many pixels we move our scott
+    step = 50
+    # check if the scott is still on screen, if not change direction
 
     Scott = pygame.image.load("SC.PNG")
     screen.blit(Scott, (ScottX, ScottY))
@@ -42,6 +42,19 @@ def main():
     while running:
         # event handling, gets all event from the eventqueue
         for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    ScottX -= step
+                    ScottX = constrain(ScottX, 0, screenWidth - step)
+                if event.key == pygame.K_RIGHT:
+                    ScottX += step
+                    ScottX = constrain(ScottX, 0, screenWidth - step)
+                if event.key == pygame.K_UP:
+                    ScottY -= step
+                    ScottY = constrain(ScottY, 0, screenHeight - step)
+                if event.key == pygame.K_DOWN:
+                    ScottY += step
+                    ScottY = constrain(ScottY, 0, screenHeight - step)
             # only do something if the event if of type QUIT
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
@@ -50,19 +63,10 @@ def main():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 running = False
 
-        # check if the smily is still on screen, if not change direction
-        if ScottX > screenWidth - 50 or ScottX < 0:
-            stepX = -stepX
-        if ScottY > screenHeight - 50 or ScottY < 0:
-            stepY = -stepY
-        # update the position of the smily
-        ScottX += stepX  # move it to the right
-        ScottY += stepY  # move it down
-
-        # now blit the smily on screen
-        screen.blit(background, (0,0))
+        # now blit the scott on screen
+        screen.blit(background, (0, 0))
         Jager = pygame.image.load("Jager.PNG")
-        screen.blit(Jager, (100, 400))
+        screen.blit(Jager, (650, 500))
 
         screen.blit(Scott, (ScottX, ScottY))
         # and update the screen (dont forget that!)
@@ -70,7 +74,7 @@ def main():
 
         # this will slow it down to 10 fps, so you can watch it,
         # otherwise it would run too fast
-        clock.tick(10)
+        clock.tick(50)
 
 
 # run the main function only if this module is executed as the main script
